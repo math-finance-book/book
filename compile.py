1124a767b5787148cdfeb757b804d8218770fff0
Kerry Back <kerryback@gmail.com>
2024-09-04 00:05:32 -0500

    retitled chapter 6

--- compile.py
+++ compile.py
@@ -1,6 +1,6 @@
 import os
 
-chapters = [f"Chapter{i}.qmd" for i in range(1, 16)]
+chapters = [f"Chapter{i}.qmd" for i in range(1, 16) if i != 10]
 for chapter in chapters:
     os.system(
         f"quarto render c:/Users/kerry/repos/book/{chapter} --to html"
