6fde318bdbbf0aa014eba71de2eeb37d9bc2550e
Kerry Back <kerryback@gmail.com>
2024-12-27 20:58:58 -0600

    removed equation

--- compile.py
+++ compile.py
@@ -1,6 +1,6 @@
 import os
 
-chapters = [f"Chapter{i}.qmd" for i in range(1, 16) if i != 10] + ["ChapterFT.qmd"]
+chapters = [f"Chapter{i}.qmd" for i in range(1, 16)] + ["ChapterFT.qmd"]
 for _ in range(2):
     for chapter in chapters:
         os.system(
