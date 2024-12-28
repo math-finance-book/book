1389f9aa85eafa0379857b11f44f2ae552c94994
Kerry Back <kerryback@gmail.com>
2024-12-28 15:28:38 -0600

    reorganized chapters

--- compile.py
+++ compile.py
@@ -1,7 +1,9 @@
 import os
+import sys 
+num = int(sys.argv[1])
 
 chapters = [f"Chapter{i}.qmd" for i in range(1, 16)] + ["ChapterFT.qmd"]
-for _ in range(1):
+for _ in range(num):
     for chapter in chapters:
         os.system(
             f"quarto render c:/Users/kerry/repos/book/{chapter} --to html"
