7a09c028537e031c56579b60437b9084c9c0365b
Kerry Back <kerryback@gmail.com>
2024-12-28 15:05:30 -0600

    quarto light and dark

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 import os
 
 chapters = [f"Chapter{i}.qmd" for i in range(1, 16)] + ["ChapterFT.qmd"]
-for _ in range(2):
+for _ in range(1):
     for chapter in chapters:
         os.system(
             f"quarto render c:/Users/kerry/repos/book/{chapter} --to html"
