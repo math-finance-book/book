c5e9f7482cd30afeda2b23955c38acd50f48c85c
Kerry Back <kerryback@gmail.com>
2024-12-27 18:45:35 -0600

    compiled all

--- compile.py
+++ compile.py
@@ -1,11 +1,8 @@
 import os
 
-chapters = [f"Chapter{i}.qmd" for i in range(1, 16) if i != 10]
-for chapter in chapters:
-    os.system(
-        f"quarto render c:/Users/kerry/repos/book/{chapter} --to html"
-    )
-for chapter in chapters:
-    os.system(
-        f"quarto render c:/Users/kerry/repos/book/{chapter} --to html"
-    )
+chapters = [f"Chapter{i}.qmd" for i in range(1, 16) if i != 10] + ["ChapterFT.qmd"]
+for _ in range(2):
+    for chapter in chapters:
+        os.system(
+            f"quarto render c:/Users/kerry/repos/book/{chapter} --to html"
+        )
