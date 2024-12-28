e50173753acefd5713513a7b0eda1230c3ebe3a8
Kerry Back <kerryback@gmail.com>
2024-12-28 15:37:57 -0600

    cosmo dark

--- compile.py
+++ compile.py
@@ -1,6 +1,6 @@
 import os
 import sys 
-num = int(sys.argv[1])
+num = 1 if len(sys.argv) == 1 else int(sys.argv[1])
 
 chapters = [f"Chapter{i}.qmd" for i in range(1, 16)] + ["ChapterFT.qmd"]
 for _ in range(num):
