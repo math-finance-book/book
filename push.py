898de2a7a039919dbfda32b1e7b2bde38551126c
Kerry Back <kerryback@gmail.com>
2025-01-05 13:36:44 -0600

    'update'

--- push.py
+++ push.py
@@ -1,9 +1,9 @@
 import os
+os.system("git pull origin main")
 os.system("quarto render")
 os.system("copy under_construction.jpg docs")
 with open("docs/CNAME", "w") as f:
     f.write("https://book.derivative-securities.org")
-os.system("git pull origin main")
 os.system("git add .")
 os.system("git commit -m 'update'")
 os.system("git push origin main")
