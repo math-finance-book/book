ff38ee39b4cab9f9e2f735374543e4b6bc198fdc
Kerry Back <kerryback@gmail.com>
2025-01-10 00:35:36 -0600

    'update'

--- notebooks.py
+++ notebooks.py
@@ -2,21 +2,41 @@ import os
 import json
 import subprocess
 
+finished = ["Brownian"]
+finished = ["Chapter_" + f + ".qmd" for f in finished]
+
+image_path = "https://www.dropbox.com/scl/fi/6hwvdff7ajaafmkpmnp0o/under_construction.jpg?rlkey=3dex2dx86anniqoutwyqashnu&dl=1"
+construction_cell = {
+    "cell_type": "markdown",
+    "metadata": {},
+    "source": [
+        f'<img src="{image_path}" alt="Under Construction" width="400"/>\n'
+    ]
+}
+
 with open("_quarto.yml", "r") as f:
     lines = [line for line in f.readlines()  if line.strip().startswith("- Chapter")]
 chapters = [line.strip()[2:] for line in lines]
+
 numbers = [f"0{n}" for n in range(1, 10)] + [str(n) for n in range(10, len(chapters) + 1)]
 names = [c.split("_")[1].replace("qmd", "ipynb") for c in chapters]
 notebooks_out = [number + "_" + name for number, name in zip(numbers, names)]
+
 notebooks_in = [c.replace("qmd", "ipynb") for c in chapters]
 
 for chapter, notebook_in, notebook_out in zip(chapters, notebooks_in, notebooks_out):
     subprocess.run("quarto convert " + chapter, shell=True, check=True)
     with open(notebook_in, 'r') as f:
         js = json.load(f)
+    
     js['cells'] = [cell for cell in js['cells'] if cell['cell_type'] != 'markdown']
+    
     for cell in js['cells']:
         cell['source'] = [line for line in cell['source'] if not line.strip().startswith('#|')]
+    
+    if chapter not in finished:
+        js['cells'].insert(0, construction_cell)
+
     new_cell = {
        "cell_type": "markdown",
        "metadata": {},
