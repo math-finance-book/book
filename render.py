b95434eb3aba9b2757e3b3d70bd556c11eda0c4c
Kerry Back <kerryback@gmail.com>
2025-08-19 06:11:24 -0500

    AI reorg

--- render.py
+++ render.py
@@ -2,3 +2,8 @@ import os
 os.system("quarto render")
 with open("docs/CNAME", "w") as f:
     f.write("https://book.derivative-securities.org")
+
+# Add git commands to commit and push
+os.system("git add -A")
+os.system("git commit -m 'AI reorg'")
+os.system("git push")
